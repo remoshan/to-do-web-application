e30a437d441ab3a46efcdd4d2297522d21622a27
Francis Remoshan <remoshanfrancis123@gmail.com>
2026-03-16 16:20:39 +0530

    Integrate ng-zorro to the frontend
    Add ng-zorro-antd to the frontend and refactor the app to use Ant Design components and reactive forms.

--- todo-frontend/src/app/app.config.ts
+++ todo-frontend/src/app/app.config.ts
@@ -1,13 +1,23 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { routes } from './app.routes';
+import { en_US, provideNzI18n } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+
+registerLocaleData(en);
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withFetch())
-  ]
+    provideHttpClient(withFetch()),
+    provideNzI18n(en_US),
+  ],
 };
